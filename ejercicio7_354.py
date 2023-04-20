583ff16b4fd0c39b996ef4caf5a820f79dbf3fd8
laura2299 <112607589+laura2299@users.noreply.github.com>
2023-04-20 09:41:02 +0400

    Update ejercicio7

--- ejercicio7_354.py
+++ ejercicio7_354.py
@@ -2,14 +2,11 @@
 """
 Created on Thu Apr 20 08:43:25 2023
 
-@author: WINDOWS 11
+@author: Leidy
 """
 import csv
 import random
 
-# Se define la proporción de entrenamiento y prueba
-train_prop = 0.8
-test_prop = 0.2
 
 # Se carga el conjunto de datos desde un archivo CSV
 with open('C:/Users/WINDOWS 11/Documents/ds_salaries.csv', 'r') as file:
@@ -20,10 +17,16 @@ with open('C:/Users/WINDOWS 11/Documents/ds_salaries.csv', 'r') as file:
 random.shuffle(data)
 
 # Se obtiene el índice de división
-n = len(data)
-train_idx = round(train_prop * n)
+N = len(data)
+print(N)
+n_test = N // 5 #el 20% de N
+n_train = N - n_test # el 80% de N
 
-# Se divide el conjunto de datos
-train = data[:train_idx]
-test = data[train_idx:]
+#se generan dos listas una para train y otra para test
+train_indices = list(range(n_train))
+test_indices = list(range(n_train, N))
 
+#se pueden usar estas listas para acceder a los datos correspondientes
+#en el conjunto de datos
+train_data = [data[i] for i in train_indices]
+test_data = [data[i] for i in test_indices]
